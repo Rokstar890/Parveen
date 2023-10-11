696ed75c977f8cf1c45aeb49aa86d6e66aa9153b
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-11 10:39:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,11 +9,9 @@ def is_enabled(value, default):
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
         return False
     else:
-        return default
-
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '23762978'))
+API_ID = int(environ.get('API_ID', '28483301'))
 API_HASH = environ.get('API_HASH', 'eff30dac5504a8660e69bfe19f668571')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6540032672:AAHHyMD92P7Z4bi2xKmqgPmWJy1l9ix8o7E")
 
@@ -27,16 +25,16 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1216307744').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5689386887').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001896066430').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '787081931 1474382506').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '1216307744 1474382506').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001394587233')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001924425523')
 reqst_channel = environ.get('REQST_CHANNEL_ID', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
@@ -58,14 +56,14 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "15")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/netflixrequstgroup1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Netflixvilla_india')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Netflixvilla_india/46')#not added
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinemavilla0012')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/cinemav00')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/cinemav00/299')#not added
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001940288450'))
 BF_LOG = int(environ.get('BF_LOG', '0'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'netflixrequstgroup1')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ncinemavilla0012')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
